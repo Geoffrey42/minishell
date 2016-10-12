c50f902b99d7c9d9b1b1cb167a530b68fe1db419
Geoffrey GANE <ggane@e1r5p6.42.fr>
2016-10-12 11:04:45 +0200

    minishelle fonctionnel. Correction des fonctions liees a setenv, il est operationnel desormais. Il peut ajouter une nouvelle variable ou simplement en reset une si elle existe deja. Enfin ces modifs affecteront bien l'env du minishell

--- src/setenv.c
+++ src/setenv.c
@@ -6,11 +6,48 @@
 /*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/10 22:41:07 by ggane             #+#    #+#             */
-/*   Updated: 2016/10/12 09:53:17 by ggane            ###   ########.fr       */
+/*   Updated: 2016/10/12 11:02:51 by ggane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*copy_name(char *src, size_t size)
+{
+	char	*copy;
+	size_t	i;
+
+	i = 0;
+	copy = (char *)malloc(sizeof(char) * size + 1);
+	if (!copy)
+		return (NULL);
+	while (i <= size)
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+int		variables_cmp(char *to_trim, char *var1)
+{
+	char	*var2;
+	size_t	i;
+
+	var2 = NULL;
+	i = 0;
+	while (to_trim[i] != '=')
+		i++;
+	if (to_trim[i] == '=')
+		var2 = copy_name(to_trim, i);
+	if (!ft_strcmp(var1, var2))
+	{
+		free(var2);
+		return (1);
+	}
+	return (0);
+}
 
 int		check_variable_existence(char *var, char **env)
 {
@@ -19,28 +56,28 @@ int		check_variable_existence(char *var, char **env)
 	i = 0;
 	while (env[i])
 	{
-		if (ft_strnstr(env[i], var, ft_strlen(var)))
+		if (variables_cmp(env[i], var))
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-char	**reset_value(char **env, char *var)
+char	**reset_value(char **env, char *var, char *arg)
 {
 	int		i;
 
 	i = 0;
 	while (env[i])
 	{
-		if (!ft_strcmp(var, env[i]))
+		if (variables_cmp(env[i], var))
 		{
-			env[i] = ft_strdup(var);
+			env[i] = ft_strdup(arg);
 			return (env);
 		}
 		i++;
 	}
-	return (NULL);
+	return (env);
 }
 
 size_t	len_till_c(char *src, int c)
@@ -57,7 +94,7 @@ size_t	len_till_c(char *src, int c)
 	return (0);
 }
 
-int		setenv_func(char *arg, char **env)
+int		setenv_func(char *arg, char ***env)
 {
 	char	*var;
 
@@ -69,10 +106,10 @@ int		setenv_func(char *arg, char **env)
 	}
 	else
 		return (0);
-	if (check_variable_existence(var, env))
-		env = add_str_to_array(env, arg);
+	if (!check_variable_existence(var, *env))
+		*env = add_str_to_array(*env, arg);
 	else
-		env = reset_value(env, arg);
+		*env = reset_value(*env, var, arg);
 	return (0);
 }
 
@@ -85,7 +122,7 @@ int		check_letters(char *str)
 		return (1);
 	while (str[i] && str[i] != '=')
 	{
-		if (!ft_isalpha(str[i]))
+		if (ft_isalpha(str[i]))
 			return (1);
 		i++;
 	}
@@ -99,13 +136,13 @@ int		execute_setenv(t_shell *info)
 	i = 1;
 	while (info->args[i])
 	{
-		if (check_letters(info->args[i]))
+		if (!check_letters(info->args[i]))
 		{
 			print_invalid_identifier(info->args[i]);
 			return (1);
 		}
 		else
-			setenv_func(info->args[i], info->env);
+			setenv_func(info->args[i], &info->env);
 		i++;
 	}
 	return (0);
