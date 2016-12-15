e67be91b6ac1261f0e0aa5c5c06477638a85aaf5
Geoffrey GANE <ggane@e1r5p6.42.fr>
2016-12-15 12:37:42 +0100

    minishell fonctionnel. Correction leaks sur setenv used alone

--- src/ft_setenv.c
+++ src/ft_setenv.c
@@ -6,11 +6,16 @@
 /*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 16:23:18 by ggane             #+#    #+#             */
-/*   Updated: 2016/12/07 15:08:52 by ggane            ###   ########.fr       */
+/*   Updated: 2016/12/15 12:36:31 by ggane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/*void	modify_permanent_env(t_data **data)
+{
+
+}*/
 
 int		ft_setenv(t_data *data)
 {
@@ -22,6 +27,9 @@ int		ft_setenv(t_data *data)
 		ascii_env = copy_env(data);
 		merge_sort(&ascii_env);
 		display_env_list(ascii_env);
+		delete_list(&ascii_env);
 	}
+	//else if (data->ac > 1 && check_setenv_errors(data))
+	//	modify_permanent_env(&data);
 	return (0);
 }
