e8360fd82e384a40cf71dea04e78e9d936c9ec5c
Geoffrey GANE <ggane@e1r5p6.42.fr>
2016-12-16 09:19:46 +0100

    minishell fonctionnel. Probleme avec setenv fix. Ready to pass final tests before push.

--- src/check_setenv_errors.c
+++ src/check_setenv_errors.c
@@ -6,7 +6,7 @@
 /*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 13:00:37 by ggane             #+#    #+#             */
-/*   Updated: 2016/12/15 21:26:20 by ggane            ###   ########.fr       */
+/*   Updated: 2016/12/16 09:18:26 by ggane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,8 @@
 
 int		check_alpha(char *arg)
 {
-	int		i;
-
-	i = 0;
-	while (arg[i])
-	{
-		if (!ft_isalpha(arg[i]))
-			return (1);
-		i++;
-	}
+	if (!ft_isalpha(arg[0]))
+		return (1);
 	return (0);
 }
 
@@ -31,17 +24,14 @@ int		check_setenv_errors(t_data *data)
 	int		i;
 
 	i = 0;
-	ft_putstr("check_setenv_errors() returns : ");
 	while (data->args[i])
 	{
 		if (check_alpha(data->args[i]))
 		{
 			print_invalid_identifier(data->args[1], data->args[0]);
-			ft_putendl("0");
 			return (0);
 		}
 		i++;
 	}
-	ft_putendl("1");
 	return (1);
 }
